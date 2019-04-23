216e9d91a2523358194c4a3df309c3b56043affd
Rebecca Ngo <40474210+Rebeccango@users.noreply.github.com>
2019-04-22 22:16:01 -0400

    started Modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// components
+import Modal from './components/Modal';
+
 class App extends Component {
+  state = {
+    show: false
+};
+
+showModal = e =>{ 
+    this.setState({
+        show: true
+    });
+};
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Reduce Reuse ReCycle React Components</h1>
+        <button onClick ={e =>{this.showModal();}}>show Modal</button>
+        <Modal show={this.state.show}/>
       </div>
     );
   }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+//source : https://medium.com/swlh/build-a-simple-modal-component-with-react-1b174c3f5301
+
 export default class Modal extends React.Component{
+
     render(){
         return(
-            <div>test</div>
+            <div>test
+            </div>
         )
     }
 }
